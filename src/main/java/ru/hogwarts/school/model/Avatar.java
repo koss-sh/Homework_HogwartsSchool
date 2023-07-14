2b3fe56b13aa79befe43d0c7b603bc165b5f1eb5
koss-sh <shilinskii22@gmail.com>
2023-07-14 22:57:32 +0300

    hw is done (without @Lob)

--- src/main/java/ru/hogwarts/school/model/Avatar.java
+++ src/main/java/ru/hogwarts/school/model/Avatar.java
@@ -12,7 +12,7 @@ public class Avatar {
     private String filePath;
     private long fileSize;
     private String mediaType;
-    @Lob
+
     private byte[] data;
     @OneToOne
     private Student student;

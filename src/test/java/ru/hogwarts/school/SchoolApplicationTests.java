2e276bf9a8b8797f08c77bf7af473cd33cc0ec2f
koss-sh <shilinskii22@gmail.com>
2023-07-19 16:58:10 +0300

    initial commit

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,72 @@
 package ru.hogwarts.school;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import ru.hogwarts.school.controllers.StudentController;
+import ru.hogwarts.school.model.Student;
 
-@SpringBootTest
-class SchoolApplicationTests {
+import java.net.URI;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SchoolApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Test
+    void contextLoads() throws Exception {
+        Assertions.assertThat(studentController).isNotNull();
+    }
 
     @Test
-    void contextLoads() {
+    public void testFindByAgeBetween() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student", String.class))
+                .isNotNull();
     }
+
+    @Test
+    public void testGetStudentInfo() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/{}", String.class))
+                .isNotNull();
+    }
+
+    @Test
+    public void testFindFacultyByStudent() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/{}/faculty", String.class))
+                .isNotNull();
+    }
+
+    @Test
+    public void testGetStudentByAge() throws Exception {
+        Assertions
+                .assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/student/age/{}", String.class))
+                .isNotNull();
+    }
+
+    @Test
+    public void testCreateStudent() throws Exception {
+        Student student = new Student();
+        student.setId(4L);
+        student.setName("Bob");
+        student.setAge(16);
+
+        Assertions
+                .assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", student, String.class))
+                .isNotNull();
+    }
+
 
 }
